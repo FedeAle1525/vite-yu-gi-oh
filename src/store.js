71d39298b05b5edd6dc7dade30575edca4ef81c1
FedeAle1525 <fedeale.1525@gmail.com>
2023-03-03 17:40:30 +0100

    JS create "Store"

---
+++ src/store.js
@@ -0,0 +1,11 @@
+// 1 - Creo un Oggetto Reattivo da usare come Store
+import { reactive } from 'vue';
+
+const store = reactive({
+
+  // 2 - Inserisco Dati da salvare dentro lo Store
+  cards: []
+})
+
+// 3 - Rendo i Dati di Store accessibili esportandoli
+export default store;
